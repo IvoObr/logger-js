f2f7f15839cc09da97258ef01a1b815b751fcd10
IvoObr <ivo_obr@hotmail.com>
2021-03-15 23:11:09 +0200

    tsc-compile

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 "use strict";
-exports.__esModule = true;
-var colors_1 = require("colors");
-var fs_1 = require("fs");
-var util_1 = require("util");
-var logger = /** @class */ (function () {
-    function logger(options) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const colors_1 = __importDefault(require("colors"));
+const fs_1 = __importDefault(require("fs"));
+const util_1 = __importDefault(require("util"));
+class logger {
+    constructor(options) {
         this.logFileName = 'logger.log';
         this.writeInFile = true;
         if (typeof (options === null || options === void 0 ? void 0 : options.logFileName) === 'string') {
@@ -14,69 +17,69 @@ var logger = /** @class */ (function () {
             this.writeInFile = options === null || options === void 0 ? void 0 : options.writeInFile;
         }
     }
-    logger.prototype.inspect = function (msg) {
-        msg = util_1["default"].inspect(msg);
+    inspect(msg) {
+        msg = util_1.default.inspect(msg);
         this.printLog(msg, {
             color: 'dim',
             prefix: 'INSPECT: '
         });
-    };
-    logger.prototype.info = function (msg) {
+    }
+    info(msg) {
         this.printLog(msg, {
             color: 'white',
             prefix: 'INFO:    '
         });
-    };
-    logger.prototype.success = function (msg) {
+    }
+    success(msg) {
         this.printLog(msg, {
             color: 'green',
             prefix: 'SUCCESS: '
         });
-    };
-    logger.prototype.warn = function (msg) {
+    }
+    warn(msg) {
         this.printLog(msg, {
             color: 'yellow',
             prefix: 'WARNING: '
         });
-    };
-    logger.prototype.error = function (msg) {
+    }
+    error(msg) {
         // msg = util.inspect(msg);
         this.printLog(msg, {
             color: 'red',
             prefix: 'ERROR:   '
         });
-    };
-    logger.prototype.printLog = function (msg, level) {
-        var time = '[' + new Date().toISOString().replace('T', ' ').substring(0, 19) + ']';
-        var colorFn = colors_1["default"][level.color];
-        console.log(time + " " + colorFn(level.prefix).bold + " " + colorFn(msg));
+    }
+    printLog(msg, level) {
+        const time = '[' + new Date().toISOString().replace('T', ' ').substring(0, 19) + ']';
+        const colorFn = colors_1.default[level.color];
+        console.log(`${time} ${colorFn(level.prefix).bold} ${colorFn(msg)}`);
         if (this.writeInFile) {
-            this.writeToFile(time + " " + level.prefix + ": " + (msg += '\r\n'));
+            this.writeToFile(`${time} ${level.prefix}: ${msg += '\r\n'}`);
         }
-    };
-    logger.prototype.writeToFile = function (msg) {
+    }
+    writeToFile(msg) {
         try {
-            var fileExists = this.doFileExist();
+            const fileExists = this.doFileExist();
             if (fileExists) {
-                fs_1["default"].appendFileSync(this.logFileName, msg);
+                fs_1.default.appendFileSync(this.logFileName, msg);
             }
             else {
-                fs_1["default"].writeFileSync(this.logFileName, msg);
+                fs_1.default.writeFileSync(this.logFileName, msg);
             }
         }
         catch (error) {
             console.error(error);
         }
-    };
-    logger.prototype.doFileExist = function () {
+    }
+    doFileExist() {
         try {
-            fs_1["default"].accessSync(this.logFileName);
+            fs_1.default.accessSync(this.logFileName);
             return true;
         }
         catch (error) {
             return false;
         }
-    };
-    return logger;
-}());
-exports["default"] = new logger();
+    }
+}
+exports.default = new logger();
+//# sourceMappingURL=index.js.map
